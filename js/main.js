e0adb620030a8d6fe9ea42e5e1511ea4ef078662
Olexandr Minenko <minenkogt@gmail.com>
2021-01-12 11:16:56 +0200

    all is done

---
+++ js/main.js
@@ -0,0 +1,9 @@
+const burger = document.querySelector(".nav__icon");
+const navList = document.querySelector('.nav__list');
+const nav = document.querySelector('.nav');
+
+burger.addEventListener('click', () => {
+   nav.classList.toggle('active');
+   navList.classList.toggle('active');
+   burger.classList.toggle('active');
+})
