97e071b6128379f1d65806be5867c69f241e011d
Olexandr Minenko <minenkogt@gmail.com>
2021-01-12 13:43:07 +0200

    new change

--- js/main.js
+++ js/main.js
@@ -1,9 +1,12 @@
-const burger = document.querySelector(".nav__icon");
-const navList = document.querySelector('.nav__list');
-const nav = document.querySelector('.nav');
+window.onload = () => {
+   const burger = document.querySelector(".nav__icon");
+   const navList = document.querySelector('.nav__list');
+   const nav = document.querySelector('.nav');
 
-burger.addEventListener('click', () => {
-   nav.classList.toggle('active');
-   navList.classList.toggle('active');
-   burger.classList.toggle('active');
-})
+   burger.addEventListener('click', () => {
+      nav.classList.toggle('active');
+      navList.classList.toggle('active');
+      burger.classList.toggle('active');
+   })
+};
+
